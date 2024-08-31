9ad11028402d63c9445a5d34a123c998651c2abf
Pulkit426 <agrawalpulkit12@gmail.com>
2024-09-01 00:32:43 +0530

    add commands config and create ping command

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({
   intents: [
@@ -11,6 +13,10 @@ client.on('messageCreate', (message) => {
   if (message.author.bot) return;
 
   message.reply({ content: 'Hi from Bot' });
+});
+
+client.on('interactionCreate', (interaction) => {
+  interaction.reply('Pong!');
 });
 
 client.login(process.env.LOGIN_TOKEN);
